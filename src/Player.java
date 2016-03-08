68cd78e1d919c4eb4bda7e1554ca5f3ee84626e7
Taylor Scafe <scafetaylor@gmail.com>
2016-03-08 12:53:31 -0600

    Set up basic GUI
    Removed Old text based functions. Updates functions to handle JButtons
    replacing the text.

--- src/Player.java
+++ src/Player.java
@@ -7,25 +7,17 @@ import java.util.ArrayList;
 
 public class Player {
 	private int score;
-	private int number;
 	ArrayList<Tile> myTiles = new ArrayList<Tile>();
 	
 	
-	public Player(int number){
-		
-		this.setNumber(number);
+	public Player(){
+		setScore(0);
 	}
 	public int getScore() {
 		return score;
 	}
 	public void setScore(int score) {
 		this.score = score;
-	}
-	public int getNumber() {
-		return number;
-	}
-	public void setNumber(int number) {
-		this.number = number;
 	}
 	public void addTile(Tile input ){
 		myTiles.add(input);
--- src/ScrabbleGUIMain.java
+++ src/ScrabbleGUIMain.java
@@ -10,13 +10,27 @@ import javax.swing.JLabel;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.JTextPane;
 
 public class ScrabbleGUIMain extends JFrame {
-
+	public JButton[][] boardList;
 	private JPanel contentPane;
-
+	public Board scrabbleBoard = new Board();
+	public Player[] playerList;
+	public Player currentPlayer = new Player();
+	public JButton selectedTile;
+	public JPanel board;
+	public JPanel selections;
+	private JRadioButton rdbtnExchange;
+	private JRadioButton rdbtnPlace;
+	private JButton btnExchange;
+	private JLabel lblInstructions;
+	private ArrayList<JButton> rack = new ArrayList<JButton>();
+	private JPanel rackPanel;
 	/**
 	 * Launch the application.
 	 */
@@ -47,23 +61,89 @@ public class ScrabbleGUIMain extends JFrame {
 		JLabel lblWelcomeToScrabble = new JLabel("Welcome to Scrabble");
 		contentPane.add(lblWelcomeToScrabble, BorderLayout.NORTH);
 		
-		JPanel board = new JPanel();
+		board = new JPanel();
 		contentPane.add(board, BorderLayout.CENTER);
 		board.setLayout(new GridLayout(15, 15, 0, 0));
-		JButton[][] boardList = new JButton[15][15];
+		boardList = new JButton[15][15];
 		for (JButton[] boardRow:boardList){
-		for (JButton boardTile:boardRow){
-			boardTile = new JButton("");
-			boardTile.setBackground(Color.white);
-			boardTile.addActionListener(new ButtonPress());
-			board.add(boardTile);
+			for (JButton boardTile:boardRow){
+				boardTile = new JButton("");
+				boardTile.setBackground(Color.white);
+				boardTile.addActionListener(new TilePress());
+				board.add(boardTile);
+			}
 		}
+		
+		selections = new JPanel();
+		contentPane.add(selections, BorderLayout.EAST);
+		selections.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		rdbtnPlace = new JRadioButton("Place");
+		selections.add(rdbtnPlace);
+		
+		rdbtnExchange = new JRadioButton("Exchange");
+		selections.add(rdbtnExchange);
+		
+		btnExchange = new JButton("Execute Exchange");
+		selections.add(btnExchange);
+		
+		
+		lblInstructions = new JLabel("Hello, I am the instruction Label");
+		contentPane.add(lblInstructions, BorderLayout.WEST);
+		
+		rackPanel = new JPanel();
+		rackPanel.setLayout(new GridLayout(1,0,0,0));
+		contentPane.add(rackPanel, BorderLayout.SOUTH);
+		
+		scrabbleBoard.getTiles(currentPlayer);
+		for (Tile tile : currentPlayer.getTiles()){
+			rackPanel.add(createButton(tile));
 		}
-		board.setEnabled(false);
+		rackUpdate();
+		contentPane.repaint();
+		
 	}
-	private class ButtonPress implements ActionListener{
+	private JButton createButton(Tile tile) {
+		JButton temp = new JButton();
+		temp.setText(tile.getLetter()+"");
+		temp.setBackground(Color.orange);
+		temp.addActionListener(new rackPress());
+		return temp;
+	}
+	private class TilePress implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent ea) {
+		switchColor((JButton) ea.getSource());	
+		}
+	}
+	private class rackPress implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent ea) {
+			selectedTile = (JButton) ea.getSource();
+			switchColor(selectedTile);
+			System.out.println(selectedTile.getText());
+			
+		}
+	}
+	public void switchColor(JButton input) {
+		if (input.getBackground()==Color.orange){
+			input.setBackground(Color.green);
+		}
+		else if(input.getBackground()==Color.green){
+			input.setBackground(Color.orange);
+		}
+		else if (input.getBackground()==Color.white){
+			input.setBackground(Color.yellow);
+		}
+		else if (input.getBackground()==Color.yellow){
+			input.setBackground(Color.white);
+		}
+			
+	}
+	
+	private void rackUpdate(){
+		for(JButton slot: rack){
+			contentPane.add(slot, BorderLayout.SOUTH);
 		}
 	}
 }
