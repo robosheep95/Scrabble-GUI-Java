af893aa539410e921298110d7d5554c2985ce9f9
sfinger <sfinger@sting.graceland.edu>
2016-03-10 22:33:42 -0600

    Final Upload 3/10
    Finished Game

--- src/GUI.java
+++ src/GUI.java
@@ -9,9 +9,12 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 import javax.swing.JButton;
 import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
 import java.awt.Toolkit;
 
 
@@ -27,7 +30,9 @@ public class GUI extends JFrame {
 	private JPanel eastPanel, westPanel, centerPanel, southPanel;
 	private JRadioButton rdbtnExchange;
 	private JRadioButton rdbtnPlace;
-	private JButton btnExchange;
+	private JButton btnExchange, nextTurnBtn;
+	private JTextField txtField;
+	private JLabel askNumPlayers;
 	
 	
 	public GUI(){
@@ -84,10 +89,17 @@ public class GUI extends JFrame {
 		eastPanel.add(rdbtnExchange);
 		
 		btnExchange = new JButton("Execute Exchange");
+		btnExchange.setEnabled(false);
 		eastPanel.add(btnExchange);
 		btnExchange.addActionListener(new exchangeExecute());
 		
+		//this is where startup begins, i only have
+		//westPanelRedraw(); here because then it paints it so I can see what i'm doing
+		//i dont know how to redraw it otherwise
 		
+		
+		//westPanelRedraw();
+		//startUp();
 		setBounds(0, 0, xSize, (ySize - 50));
 		setVisible(true);
 		repaint();
@@ -102,15 +114,24 @@ public class GUI extends JFrame {
 		public void actionPerformed(ActionEvent ea) {
 			JButton input =  (JButton) ea.getSource();
 			if(rdbtnPlace.isSelected() && input.getBackground() == Color.WHITE){
-				System.out.println("test");
 				for(int i = 0;i<southPanel.getComponentCount();i++){
 					if (southPanel.getComponent(i).getBackground() == Color.green){
 						ArrayList<Tile> currentTiles = playerList[intCurrentPlayer].getTiles();
 						input.setText(currentTiles.get(i).getLetter()+"");
 						input.setBackground(Color.yellow);
+						playerList[intCurrentPlayer].setScore(playerList[intCurrentPlayer].getScore() + currentTiles.get(i).getValue());
 						playerList[intCurrentPlayer].removeTile(i);
+						
+						for(int j=0;j<centerPanel.getComponentCount();j++){
+							JButton test = (JButton) centerPanel.getComponent(j);
+							if(input.equals(test)){
+								scrabbleBoard.setSpace(j, input.getText().charAt(0));
+							}
+						}
+						
 						southPanel.remove(input);
 						rackUpdate();
+						System.out.println("trigger");
 						boolCanExchange = false;
 						break;
 					}	
@@ -141,9 +162,11 @@ public class GUI extends JFrame {
 					else
 						i++;
 				}
+				scrabbleBoard.getTiles(playerList[intCurrentPlayer]);
+				nextTurnBtn.doClick();
+				rdbtnPlace.doClick();
 			}
-			scrabbleBoard.getTiles(playerList[intCurrentPlayer]);
-			rackUpdate();
+			
 		}
 	}
 	private class radioClick implements ActionListener{
@@ -162,18 +185,55 @@ public class GUI extends JFrame {
 			}
 		}
 	}
-	private class nextPlayerClick implements ActionListener{
+
+	private class txtFieldButton implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent ea) {
-			if (intCurrentPlayer == playerList.length-1){
+			try{
+				int temp = Integer.parseInt(txtField.getText());
+				
+				playerList = new Player[temp];
+				
+				if(temp<1){
+					System.err.println("Illegal input");
+				}
+				else{
+					for(int i=0; i < temp; i++){
+						playerList[i] = new Player();
+						scrabbleBoard.getTiles(playerList[i]);
+						//System.out.println("worked");	
+					}
+					intCurrentPlayer = 0;
+					btnExchange.setEnabled(true);
+					westPanelRedraw();
+					rackUpdate();
+					//System.out.println(playerList.length);
+				}
+			}
+			catch(NumberFormatException ex){
+				System.err.println("Illegal input");
+			}
+
+		}
+	}
+
+	private class changePlayers implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent ea) {
+			scrabbleBoard.getTiles(playerList[intCurrentPlayer]);
+			if(intCurrentPlayer == playerList.length-1){
 				intCurrentPlayer = 0;
 			}
 			else{
-				
+				intCurrentPlayer++;
 			}
+			boolCanExchange=true;
+			westPanelRedraw();
+			boardUpdate();
+			rackUpdate();
+			
 		}
 	}
-	
 //--------------------------------------------------------------------------------------------
 	
 //Functions
@@ -213,24 +273,48 @@ public class GUI extends JFrame {
 				centerPanel.add(temp);
 			}	
 		}
-		boolCanExchange=true;
+		
 		centerPanel.revalidate();
 		centerPanel.repaint();
 		repaint();
 	}
-	public void startUp(){//Saliva
-		//ask # of players
-		//create players and add to list
-		// set instructions
+	public void startUp(){
+		JLabel askNumPlayers = new JLabel("How many players will there be? ");
+		//not sure how to make the text box any smaller 
+		txtField = new JTextField(1);
+		westPanel.add(askNumPlayers, BorderLayout.WEST);
+		westPanel.add(txtField, BorderLayout.WEST);
+		txtField.addActionListener(new txtFieldButton());
+		boolCanExchange = true;
 	}
 	
 	
 	public void westPanelRedraw(){
-		//wipe startup west panel
-		//display current player
-		//display all players score
+		westPanel.removeAll();
 		
-		//draw next turn button in constructor
+		//westPanel = new JPanel();
+		//contentPane.add(westPanel, BorderLayout.WEST);
+		//westPanel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JLabel displayPlayer = new JLabel("Player " + (intCurrentPlayer+1) + "'s turn.");
+		westPanel.add(displayPlayer);
+		
+		//displays players scores
+		//it was throwing a NullPointerException
+		for(int i=0; i<playerList.length; i++){
+			JLabel displayScores = new JLabel();
+			displayScores.setText("Player " + (i+1) + ": " + playerList[i].getScore());
+			westPanel.add(displayScores);
+		}
+		
+		//dont know how to repaint when the function isnt placed in the constructor
+		
+		nextTurnBtn = new JButton("Next Player");
+		westPanel.add(nextTurnBtn);
+		nextTurnBtn.addActionListener(new changePlayers());
+		westPanel.revalidate();
+		westPanel.repaint();
+
 	}
 	
 	public void rackUpdate(){
@@ -246,5 +330,6 @@ public class GUI extends JFrame {
 		southPanel.revalidate();
 		southPanel.repaint();
 	}
+
 	
 }//LAST BRACKET DO NOT REMOVE!!!!!!!
